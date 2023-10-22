9a07e22378ef54bcff31b50e9cff0faff234c485
xavero-evandro <evandromelos@hotmail.com>
2023-10-22 22:26:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# DDD (Domain-drive Design)
+
+## Domain
+
+- Domain Experts
+  - Conversations
+- Ubiquitous language
+
+- Users
+  - Client
+  - Providers
+  - Attendant
+  - Barman(Mixologist)
+
+- Aggregators
+- Value Objects
+- Domain Events
+- Sub Domains (Bounded Contexts)
+- Entities
+- Use Cases
---
+++ src/core/events/domain-events.spec.ts
@@ -0,0 +1,47 @@
+import { DomainEvent } from '../events/domain-event'
+import { UniqueEntityID } from '../entities/unique-entity-id'
+import { AggregateRoot } from '../entities/aggregate-root'
+import { DomainEvents } from '@/core/events/domain-events'
+import { vi } from 'vitest'
+
+class CustomAggregateCreated implements DomainEvent {
+  public ocurredAt: Date
+  private aggregate: CustomAggregate // eslint-disable-line
+
+  constructor(aggregate: CustomAggregate) {
+    this.aggregate = aggregate
+    this.ocurredAt = new Date()
+  }
+
+  public getAggregateId(): UniqueEntityID {
+    return this.aggregate.id
+  }
+}
+
+class CustomAggregate extends AggregateRoot<null> {
+  static create() {
+    const aggregate = new CustomAggregate(null)
+
+    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
+
+    return aggregate
+  }
+}
+
+describe('domain events', () => {
+  it('should be able to dispatch and listen to events', async () => {
+    const callbackSpy = vi.fn()
+
+    DomainEvents.register(callbackSpy, CustomAggregateCreated.name)
+
+    const aggregate = CustomAggregate.create()
+
+    expect(aggregate.domainEvents).toHaveLength(1)
+
+    DomainEvents.dispatchEventsForAggregate(aggregate.id)
+
+    expect(callbackSpy).toHaveBeenCalled()
+
+    expect(aggregate.domainEvents).toHaveLength(0)
+  })
+})
